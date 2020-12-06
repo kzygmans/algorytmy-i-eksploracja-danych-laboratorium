5e6a3574a8655f6e28a97ec80baf38fd19f8f646
Karol Zygmanski <kzygmans@gmail.com>
2020-12-06 19:41:00 +0100

    Lab2 - Exercise 3

--- lab2/main.py
+++ lab2/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 global data
 
@@ -49,24 +50,53 @@ def zadanie2():
     print(dataset2)
     print(pca_data)
     centroids = pd.DataFrame(pca_2d.fit_transform(kMeansPP.cluster_centers_))
-    subplot1 = centroids.plot.scatter(0, 1, marker='*', s=150, linewidths=2, color='red', zorder=8, label='Centoridy')
+    subplot2 = centroids.plot.scatter(0, 1, marker='*', s=150, linewidths=2, color='red', zorder=8, label='Centoridy')
     markers = ['o', 'v', 's', 'h', 'p']
     colors = ['goldenrod', 'yellowgreen', 'skyblue', 'plum', 'silver']
     for i in range(number_of_clusters):
         v = pca_data.loc[pca_data['cluster'] == i]
-        subplot1.scatter(v[0], v[1], color=colors[i], marker=markers[i], label='Skupienie ' + str(i))
-    subplot1.legend(loc='upper right')
-    subplot1.grid(True)
+        subplot2.scatter(v[0], v[1], color=colors[i], marker=markers[i], label='Skupienie ' + str(i))
+    subplot2.legend(loc='upper right')
+    subplot2.grid(True)
     plt.title("KMeans++")
     plt.savefig("plot_z2_2.png")
 
-    plt.show()
 
 def zadanie3():
     # Parameters
-    metric = ''
+    metric = 'euclidean'
+
+    # AgglomerativeClustering
+    dataset3 = data.drop(data.iloc[:, :1], inplace=False, axis=1)
+    agglomerative = AgglomerativeClustering(n_clusters=5, affinity=metric, linkage='ward')
+    agglomerative.fit(dataset3)
+    print(agglomerative)
+    print(agglomerative.labels_)
+    dataset3['cluster'] = agglomerative.labels_
+    print(dataset3)
+    g = sns.clustermap(dataset3, metric=metric)
+    g.savefig("plot_z3_1.png")
+
+    # DBSCAN
+    dataset4 = data.drop(data.iloc[:, :1], inplace=False, axis=1)
+    pca_2d = PCA(n_components=2)
+    pca_data4 = pd.DataFrame(pca_2d.fit_transform(dataset4))
+    dbscan = DBSCAN(metric=metric, eps=0.15, min_samples=10)
+    dbscan.fit(pca_data4)
+    print(dbscan.labels_)
+    pca_data4['cluster'] = dbscan.labels_
+    subplot4 = plt.subplot()
+    c1 = pca_data4.loc[pca_data4['cluster'] == 0]
+    subplot4.scatter(c1[0], c1[1], color='blue', marker='*', label='Skupienie 1')
+    c2 = pca_data4.loc[pca_data4['cluster'] == 1]
+    subplot4.scatter(c2[0], c2[1], color='red', marker='^', label='Skupienie 2')
+    subplot4.legend(loc='upper right')
+    subplot4.grid(True)
+    plt.savefig("plot_z3_2.png")
+    plt.show()
+
 
 if __name__ == "__main__":
     zadanie1()
     zadanie2()
-
+    zadanie3()
